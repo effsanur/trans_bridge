ca409ed9f91e527408cbaa3925b7eebda2db13dc
ikbalgokce <ikbalgokce41@gmail.com>
2025-07-17 15:06:28 +0300

    feat: Isaret dili ceviri ozelligi entegrasyonu ve son iyilestirmeler, venv .gitignore'a eklendi

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart'; // Widget'ı import edin
 import 'package:go_router/go_router.dart';
+import 'package:flutter/material.dart'; // Material Design widget'ları için
+
 import 'package:trans_bridge/views/app_view.dart';
 import 'package:trans_bridge/views/home_view/home_view.dart';
 import 'package:trans_bridge/views/live_support_view/live_support_view.dart';
 import 'package:trans_bridge/views/past_view/past_view.dart';
 import 'package:trans_bridge/views/profile_view/profile_view.dart';
+import 'package:trans_bridge/features/sign_language_translation/sign_language_translation_page.dart'; // Yeni sayfanızı içe aktarın
 
 final _routerKey = GlobalKey<NavigatorState>();
 
@@ -15,6 +18,7 @@ class AppRoutes {
   static const String past = '/past';
   static const String livesupport = '/livesupport';
   static const String profile = '/profile';
+  static const String signLanguage = '/sign_language'; // Yeni rota eklendi
 }
 
 final router = GoRouter(
@@ -58,6 +62,13 @@ final router = GoRouter(
           ],
         ),
       ],
+    ),
+    // Yeni işaret dili çeviri sayfasını doğrudan routes listesine ekle
+    GoRoute(
+      path: AppRoutes.signLanguage,
+      builder: (BuildContext context, GoRouterState state) {
+        return const SignLanguageTranslationPage();
+      },
     ),
   ],
 );
